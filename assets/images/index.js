34dea8a5daa777ffab3a4ce62b8d7ce5c4a4d6a1
River <ahe794613@gmail.com>
2022-05-19 17:46:48 +0800

    remove preloadImages in index

--- assets/images/index.js
+++ assets/images/index.js
@@ -7,14 +7,6 @@ const images = {
     anasisys: require('./background/AnalisysScreen.jpg'),
     settings: require('./background/SettingsScreen.jpg'),
   },
-  preloadImages: [
-    require('./background/UserLoginScreen.jpg'),
-    require('./background/UserRegisterScreen.jpg'),
-    require('./background/CardStackScreen.jpg'),
-    require('./background/CardListScreen.jpg'),
-    require('./background/AnalisysScreen.jpg'),
-    require('./background/SettingsScreen.jpg'),
-  ],
 };
 
 export default images;
